bc4cf818e0ccee08385fa570aec0e30a85b61fe1
Jagner Rafael Bonfim da Silva <jagnerrs@gmail.com>
2019-02-18 15:02:25 -0300

    First Commit

---
+++ MalweeTestApplication/MalweeTestApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MalweeTestApplication.Startup))]
+namespace MalweeTestApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
